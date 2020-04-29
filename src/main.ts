7cafc1a6293ddff02fd9bbb3a2e7520ff73b032d
Gabe Wyatt <gabe@solarixdigital.com>
2020-04-28 20:50:07 -0700

    Added SSL support. Added environment-specific configurations.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import configuration from 'src/config/configuration';
+import config from 'src/config/config';
+import * as fs from 'fs';
 
 async function bootstrap() {
+  let httpsOptions = null;
+  if (config.get('security.ssl.enabled')) {
+    httpsOptions = {
+      key: fs.readFileSync(config.get('security.ssl.key')),
+      cert: fs.readFileSync(config.get('security.ssl.certificate'))
+    };
+  }
+
+  console.log(config.get('env'));
+
   const app = await NestFactory.create(AppModule, {
-    bodyParser: false
+    bodyParser: false,
+    httpsOptions
   });
+
   app.setGlobalPrefix(configuration().routes.prefix);
 
   // Add Redis microservice

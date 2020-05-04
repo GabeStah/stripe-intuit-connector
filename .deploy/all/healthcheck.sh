9c5e2ccdaf0196fdb2452014be4737b1116e9ec5
Gabe Wyatt <gabe@solarixdigital.com>
2020-05-04 01:20:52 -0700

    Ignoring TLS for curl health check.

--- .deploy/all/healthcheck.sh
+++ .deploy/all/healthcheck.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-# Add curl
-apk add curl
-
 # Perform basic up health check
-if curl -s --head --request GET "${APP_URL}"/v1/healthcheck | grep "200 OK" > /dev/null; then
+if curl -k --request GET "${APP_URL}"/v1/healthcheck | grep "200 OK" > /dev/null; then
   echo "Health check passed."
   exit 0
 else

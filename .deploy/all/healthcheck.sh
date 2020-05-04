c560a04d8f62dfca054e15f504313d1d2d6b3230
Gabe Wyatt <gabe@solarixdigital.com>
2020-05-04 01:00:59 -0700

    Add curl to health check.

--- .deploy/all/healthcheck.sh
+++ .deploy/all/healthcheck.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Add curl
+apk add curl
+
 # Perform basic up health check
 if curl -s --head --request GET "${APP_URL}"/v1/healthcheck | grep "200 OK" > /dev/null; then
   echo "Health check passed."
